9f946fee1c3b12327a6a2022f10cc046c8c92db9
Jeongpil <gogoj2k@gmail.com>
2021-07-13 11:48:52 +0900

    first commit

---
+++ client/src/components/views/LandingPage/Sections/MainImage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function MainImage(props){
+    return(
+        <div style={{background:`url('${props.image}')`,
+        height:'500px' ,
+        width: '80%',
+        backgroundSize: '100%, cover',
+        backgroundPosition: 'center, center',
+        width: '100%',
+        position: 'relative'
+        }}>
+            <div>
+                <div style={{position:'absolute', maxWidth:'500px', bottom:'2rem', marginLeft:'2rem'}}>
+                    <h2 style={{color:'white'}}> {props.title} </h2>
+                    <p style={{color:'white', fontSize:'1'}}>{props.desc}</p>
+                </div>                
+            </div>
+        </div>
+
+    )
+}
+
+export default MainImage
